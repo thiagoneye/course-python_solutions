fafcda03c5a223925221645ddffdac12073a9ee6
Thiago Rodrigues <57311069+thiagoneye@users.noreply.github.com>
2023-02-20 15:36:47 -0300

    Update 01-API Consumption/API + Report.ipynb and Upload 02-Dashboard Development

---
+++ 02-Dashboard Development/main.py
@@ -0,0 +1,16 @@
+"""
+Python Solutions Week
+Dashboard development
+"""
+
+# Imports
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import pandas_datareader.data as wb
+import plotly.graph_objs as go
+
+# Main
+
+st.sidebar.title('Menu')
